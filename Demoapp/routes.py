94d3089223573b258a4761e715b4b9dd9dc0ac4f
JMSajib <jm.sajib012@gmail.com>
2018-09-29 00:27:08 +0600

    Update and Delete features are working

--- Demoapp/routes.py
+++ Demoapp/routes.py
@@ -1,7 +1,7 @@
 import secrets
 import os
 from PIL import Image
-from flask import render_template,url_for,flash,redirect,request
+from flask import render_template,url_for,flash,redirect,request,abort
 from Demoapp import app,db,bcrypt
 from Demoapp.forms import RegistrationForm,LoginForm,UpdateAccountForm,PostForm
 from Demoapp.models import Post,User
@@ -111,4 +111,43 @@ def new_post():
         db.session.commit()
         flash('Post has been Created Successfully','success')
         return redirect(url_for('home'))
-    return render_template('new_post.html',title='New Post',form=form)
+    return render_template('new_post.html',title='New Post',legend="Create Post",form=form)
+
+
+@app.route('/post/<int:post_id>')
+def detail_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('detail_post.html',title=post.title,post=post)
+
+
+@app.route('/post/<int:post_id>/update',methods=['GET','POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash("Your Post has been Updated",'success')
+        return redirect(url_for('detail_post',post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+
+    return render_template('update_post.html',title="Update Post",form=form)
+
+
+@app.route('/delete/<int:post_id>',methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash("Your Post has been Deleted",'danger')
+    return redirect(url_for('home'))    
